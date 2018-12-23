a11859dbfc8e949b592570bf3b558e7a27da5966
jbautistam <jbautistam@gmail.com>
2018-12-23 12:48:15 +0100

    First version

---
+++ src/Files/WindowsFiles.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Bau.Libraries.LibSystem.Files
+{
+	/// <summary>
+	///		Clase de ayuda para tratamiento de procesos especiales con archivos en Windows
+	/// </summary>
+	public static class WindowsFiles
+	{
+		/// <summary>
+		///		Elimina el archivo y lo guarda en la papelera de reciclaje
+		/// </summary>
+		public static bool KillFileForRecycle(string fileName)
+		{ 
+			try
+			{ 
+				API.ShellFileOperationApi.SHFILEOPSTRUCT shf = new API.ShellFileOperationApi.SHFILEOPSTRUCT();
+
+					// Inicializa las propiedades
+					shf.wFunc = API.ShellFileOperationApi.FO_DELETE;
+					shf.fFlags = API.ShellFileOperationApi.FOF_ALLOWUNDO | API.ShellFileOperationApi.FOF_NOCONFIRMATION;
+					shf.pFrom = fileName;
+					// Ejecuta el método
+					API.ShellFileOperationApi.SHFileOperation(ref shf);
+					// Indica que se ha borrado correctamente
+					return true;
+			}
+			catch
+			{ 
+				return false;
+			}
+		}
+
+		/// <summary>
+		///		Abre un documento utilizando el shell
+		/// </summary>
+		public static void OpenDocumentShell(string executable, string fileName)
+		{ 
+			new Processes.SystemProcessHelper().ExecuteApplication(executable, fileName);
+		}
+		
+		/// <summary>
+		///		Abre el documento utilizando el shell
+		/// </summary>
+		public static void OpenDocumentShell(string fileName)
+		{	
+			OpenDocumentShell(null, fileName);
+		}
+
+		/// <summary>
+		///		Abre el explorador con un archivo
+		/// </summary>
+		public static void OpenBrowser(string url)
+		{ 
+			OpenDocumentShell("iexplore.exe", url);
+		}
+
+		/// <summary>
+		///		Ejecuta una aplicación
+		/// </summary>
+		public static void ExecuteApplication(string executable)
+		{ 
+			OpenDocumentShell(executable, null);
+		}
+
+		/// <summary>
+		///		Ejecuta una aplicación
+		/// </summary>
+		public static void ExecuteApplication(string executable, string arguments) 
+		{ 
+			OpenDocumentShell(executable, arguments);
+		}
+	}
+}
---
+++ src/Processes/SystemProcessHelper.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace Bau.Libraries.LibSystem.Processes
+{
+	/// <summary>
+	///		Clase de ayuda para el tratamiento de procesos (ejecutables)
+	/// </summary>
+	public class SystemProcessHelper
+	{
+		/// <summary>
+		///		Ejecuta la aplicación asociada a un tipo de documento
+		/// </summary>
+		public void ExecuteApplicationForFile(string fileName)
+		{
+			ExecuteApplication(null, fileName);
+		}
+
+		/// <summary>
+		///		Ejecuta una aplicación pasándole los argumentos especificados
+		/// </summary>
+		public bool ExecuteApplication(string executable, string arguments = null, bool checkPrevious = false)
+		{
+			bool blnExecuted = false;
+
+				// Ejecuta la aplicación
+				if (!checkPrevious || !CheckProcessing(executable))
+				{
+					Process process = new Process();
+
+						// Inicializa las propiedades del proceso
+						process.StartInfo.UseShellExecute = true;
+						process.StartInfo.RedirectStandardOutput = false;
+						if (string.IsNullOrEmpty(executable))
+						{
+							process.StartInfo.FileName = arguments ?? "";
+							process.StartInfo.Arguments = "";
+						}
+						else
+						{
+							process.StartInfo.FileName = executable;
+							process.StartInfo.Arguments = arguments ?? "";
+						}
+						process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+						process.StartInfo.CreateNoWindow = true;
+						// Ejecuta el proceso
+						blnExecuted = process.Start();
+				}
+				// Devuelve el valor que indica si se ha ejecutado
+				return blnExecuted;
+		}
+
+		/// <summary>
+		///		Comprueba si se está procesando una aplicación
+		/// </summary>
+		public bool CheckProcessing(string executable)
+		{
+			return GetActiveProcesses(executable).Count > 0;
+		}
+
+		/// <summary>
+		///		Obtiene los procesos activos de un ejecutable
+		/// </summary>
+		public List<Process> GetActiveProcesses(string executable)
+		{
+			List<Process> objColProcess = new List<Process>();
+
+				// Obtiene los procesos del ejecutable
+				if (!string.IsNullOrEmpty(executable))
+				{
+					Process[] arrProcesses = Process.GetProcesses();
+					string name = System.IO.Path.GetFileNameWithoutExtension(executable);
+
+						// Recorre los procesos comprobando el nombre de archivo y/o el nombre de proceso
+						foreach (Process process in arrProcesses)
+							if (process.ProcessName.Equals(name, StringComparison.CurrentCultureIgnoreCase) ||
+									process.StartInfo.FileName.Equals(executable, StringComparison.CurrentCultureIgnoreCase))
+								objColProcess.Add(process);
+				}
+				// Devuelve la colección de procesos
+				return objColProcess;
+		}
+
+		/// <summary>
+		///		Elimina un proceso de memoria
+		/// </summary>
+		public bool Kill(Process process, out string error)
+		{ 
+			// Inicializa los argumentos de salida
+			error = "";
+			// Elimina el proceso de memoria
+			try
+			{
+				process.Kill();
+			}
+			catch (Exception exception)
+			{
+				error = exception.Message;
+			}
+			// Devuelve el valor que indica si se ha eliminado el proceso
+			return string.IsNullOrWhiteSpace(error);
+		}
+	}
+}
